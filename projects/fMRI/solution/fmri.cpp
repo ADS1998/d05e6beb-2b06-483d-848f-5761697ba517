KCL-BMEIS/OOP

Load the entire image series
Now that we have an Image class to hold the data for one slice, we can
form a std::vector of such Images, and load all of the image slices. To
do this, we pass the filenames of all of the images as individual
arguments, in the correct order. Our dataset contains 170 files, and
obviously we do not want to type in 170 filenames! Thankfully, we can
use the shell's `*` wildcard character to match all the image files.
Assuming the files are sorted in alphabetical order, this allows us to
load the dataset using a command such as:

    ./fmri ../data/fmri-*.pgm

Having loaded the data, we can then extract the values for the same
pixel across each time point, and display them on the terminal.

projects/fMRI/solution/fmri.cpp
@@ -16,13 +16,28 @@ void run (std::vector<std::string>& args)
   if (args.size() < 2)
     throw std::runtime_error ("missing arguments - expected at least 1 argument");
 
-  auto slice = load_pgm (args[1]);
+  std::vector<Image> slices;
+  for (unsigned int n = 1; n < args.size(); ++n)
+    slices.push_back (load_pgm (args[n]));
 
-  std::cerr << std::format ("image \"{}\" loaded, size {}x{}\n", args[1], slice.width(), slice.height());
+  // check that dimensions all match up:
+  for (unsigned int n = 1; n < slices.size(); ++n) {
+    if ( (slices[n].width() != slices[n-1].width()) ||
+         (slices[n].height() != slices[n-1].height()) )
+      throw std::runtime_error ("dimensions do not match across slices");
+  }
 
-  int x = slice.width()/2;
-  int y = slice.height()/2;
-  std::cerr << std::format ("image value at pixel ({},{}) = {}\n", x, y, slice.get (x,y));
+  std::cerr << std::format (
+      "loaded {} slices of size {}x{}\n",
+      slices.size(), slices[0].width(), slices[0].height());
+
+  int x = slices[0].width()/2;
+  int y = slices[0].height()/2;
+
+  std::cerr << std::format ("image values at pixel ({},{}) = [ ", x, y);
+  for (const auto& slice : slices)
+    std::cerr << slice.get (x,y) << " ";
+  std::cerr << "]\n";
 }
 
 

ADS1998/d05e6beb-2b06-483d-848f-5761697ba517

projects/fMRI/solution/fmri.cpp
@@ -19,13 +19,28 @@ int main (int argc, char* argv[])
 
   try { // Start of main processing:
 
-    auto slice = load_pgm (argv[1]);
+    std::vector<Image> slices;
+    for (unsigned int n = 1; n < argc; ++n)
+      slices.push_back (load_pgm (argv[n]));
 
-    std::cerr << std::format ("image \"{}\" loaded, size {}x{}\n", argv[1], slice.width(), slice.height());
+    // check that dimensions all match up:
+    for (unsigned int n = 1; n < slices.size(); ++n) {
+      if ( (slices[n].width() != slices[n-1].width()) ||
+          (slices[n].height() != slices[n-1].height()) )
+        throw std::runtime_error ("dimensions do not match across slices");
+    }
 
-    int x = slice.width()/2;
-    int y = slice.height()/2;
-    std::cerr << std::format ("image value at pixel ({},{}) = {}\n", x, y, slice.get (x,y));
+    std::cerr << std::format (
+        "loaded {} slices of size {}x{}\n",
+        slices.size(), slices[0].width(), slices[0].height());
+
+    int x = slices[0].width()/2;
+    int y = slices[0].height()/2;
+
+    std::cerr << std::format ("image values at pixel ({},{}) = [ ", x, y);
+    for (const auto& slice : slices)
+      std::cerr << slice.get (x,y) << " ";
+    std::cerr << "]\n";
 
   } // end of main processing
 