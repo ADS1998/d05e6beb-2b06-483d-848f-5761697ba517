bee6f8da94364ce62043a286732edc257f5f26dc
ADS1998 <59927720+ADS1998@users.noreply.github.com>
2026-01-27 13:43:25 +0000

    Working on W1

---
+++ my_project/hello_world/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Hello World\n";
+  int x = 0;
+  std::cout << "x = " << x << "\n";
+  float weight = 71.23;
+  std::cout << "weight = " << weight << "\n";
+  int y = 71.23;
+  std::cout << "y = " << y << "\n";
+  unsigned int z = -5;
+  std::cout << "z = " << z << "\n";
+  int16_t a = 9000000;
+  std::cout << "a = " << a << "\n";
+  return 0;
+}
